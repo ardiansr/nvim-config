4ebaad18f0157bf9d8a4551ef2ade125b511bfe0
ardiansr <ardiansr@outlook.com>
2024-07-20 17:04:33 +0700

    add oil for manage file tree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,3 +5,6 @@ vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { })
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { })
 vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { })
 vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { })
+
+-- Oil keymaps
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
