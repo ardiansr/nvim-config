354551340ddc45892927726ffbd5a88afd84b2c8
ardiansr <ardiansr@proton.me>
2024-08-16 04:07:15 +0700

    add gitsigns plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,30 @@
 return {
-  "no-clown-fiesta/no-clown-fiesta.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("no-clown-fiesta").setup({
-      transparent = false,
-      styles = {
-        comments = { italic = false }
-      }
-    })
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({ })
+    end
+  },
+  {
+    "no-clown-fiesta/no-clown-fiesta.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("no-clown-fiesta").setup({
+        transparent = false,
+        styles = {
+          comments = { italic = false }
+        }
+      })
 
-    vim.cmd([[
-      colorscheme no-clown-fiesta
-      hi ColorColumn guibg=#202020
-      hi LineNr gui=bold guifg=#e1e1e1
-      hi LineNrAbove guifg=#373737
-      hi LineNrBelow guifg=#373737
-      hi StatusLine guibg=#171717 guifg=#e1e1e1
-    ]])
-  end
+      vim.cmd([[
+        colorscheme no-clown-fiesta
+        hi ColorColumn guibg=#202020
+        hi LineNr guifg=#e1e1e1
+        hi LineNrAbove guifg=#373737
+        hi LineNrBelow guifg=#373737
+        hi StatusLine guibg=#171717 guifg=#e1e1e1
+      ]])
+    end
+  }
 }
